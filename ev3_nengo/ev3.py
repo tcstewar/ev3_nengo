a3ef4dc5b01ec6207e321cf8317aa8f4dc0ecbaf
Terry Stewart <tcstewar@uwaterloo.ca>
2015-10-19 17:15:58 -0400

    Initial version

---
+++ ev3_nengo/ev3.py
@@ -0,0 +1,140 @@
+class EV3Device(object):
+    class_dir = None
+
+    def __init__(self, port, ev3, msg_period=0.01):
+        self.port = port
+        self.ev3 = ev3
+        self.msg_period = msg_period
+        self.find_path()
+        self.initialize()
+
+    def initialize(self):
+        pass
+
+
+class Sensor(EV3Device):
+    mode = None
+
+    def find_path(self):
+        class_path = '/sys/class/%s' % self.class_dir
+        for path in self.ev3.dir(class_path).split():
+            if path.startswith('sensor'):
+                port = self.ev3.read('%s/%s/port_name' % (class_path, path))
+                assert port.startswith('in')
+
+                if int(port[2]) == self.port:
+                    self.path = '%s/%s' % (class_path, path)
+                    self.ev3.write('%s/mode' % self.path, self.mode)
+
+class Motor(EV3Device):
+    class_dir = None
+
+    def find_path(self):
+        class_path = '/sys/class/%s' % self.class_dir
+        for path in self.ev3.dir(class_path).split():
+            if path.startswith('motor'):
+                port = self.ev3.read('%s/%s/port_name' % (class_path, path))
+                assert port.startswith('out')
+
+                if port[3] == self.port:
+                    self.path = '%s/%s' % (class_path, path)
+
+class IRProx(Sensor):
+    class_dir = 'lego-sensor'
+    mode = 'IR-PROX'
+
+    def read(self):
+        value = self.ev3.read('%s/value0' % self.path,
+                              msg_period=self.msg_period)
+        return float(value) / 100
+    
+class Ultrasonic(Sensor):
+    class_dir = 'lego-sensor'
+    mode = 'US-DIST-CM'
+
+    def read(self):
+        value = self.ev3.read('%s/value0' % self.path,
+                              msg_period=self.msg_period)
+        return float(value) / 255
+
+class TachoMotor(Motor):
+    class_dir = 'tacho-motor'
+
+    def initialize(self):
+        self.ev3.write('%s/command' % self.path, 'run-direct')
+
+    def set_power(self, value):
+        value = '%d' % max(min(int(value * 100), 100), -100)
+        self.ev3.write('%s/duty_cycle_sp' % self.path, value,
+                       msg_period=self.msg_period)
+
+    def get_position(self):
+        return self.ev3.read('%s/position' % self.path)
+
+    def set_position(self, value):
+        return self.ev3.write('%s/position' % self.path, '%d' % value)
+
+
+    def stop(self):
+        self.ev3.write('%s/duty_cycle_sp' % self.path, '0')
+
+    
+
+
+class EV3(object):
+    def __init__(self, link):
+        self.link = link
+        self.motors = {}
+        self.motor_msg_period = None
+    
+    def init_motors(self, msg_period=0.01):
+        self.motor_msg_period = msg_period
+        class_path = '/sys/class/tacho-motor'
+        for path in self.link.dir(class_path).split():
+            if path.startswith('motor'):
+                port = self.link.read('%s/%s/port_name' % (class_path, path))
+                self.link.write('%s/%s/command' % (class_path, path),
+                                'run-direct')
+                assert port.startswith('out')
+                port = port[3]
+
+                self.motors[port] = '%s/%s' % (class_path, path)
+
+    def set_motor(self, **kwargs):
+        for port, value in kwargs.items():
+            value = '%d' % max(min(int(value * 100),100),-100)
+            self.link.write('%s/duty_cycle_sp' % self.motors[port],
+                            data=value, msg_period=self.motor_msg_period)
+
+    def close(self):
+        for path in self.motors.values():
+            self.link.write('%s/duty_cycle_sp' % path, data='0', blocking=True)
+
+
+
+if __name__ == '__main__':
+    import ev3_nengo.ev3link
+
+    ev3 = ev3_nengo.ev3link.EV3Link('10.42.0.3')
+    ev3.wait_for_connection()
+
+    motor = TachoMotor('A', ev3, msg_period=0.1)
+    irprox = IRProx(1, ev3, msg_period=0.1)
+    us = Ultrasonic(3, ev3, msg_period=0.1)
+
+    data = []
+    t = []
+    import time
+    start = time.time()
+    while time.time() - start < 3:
+        motor.set_power((time.time()-start) % 1.0)
+        data.append((irprox.read(), us.read()))
+        t.append(time.time()-start)
+        print data[-1]
+    print 'elapsed', time.time()-start
+
+    motor.stop()
+
+    import pylab
+    pylab.plot(t, data)
+    pylab.show()
---
+++ ev3_nengo/ev3link.py
@@ -0,0 +1,128 @@
+import math
+import socket
+import struct
+import threading
+import time
+import timeit
+
+import numpy as np
+
+class EV3Link(object):
+    def __init__(self, addr=None, port=8800):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.socket.bind(('', port))
+        if addr is None:
+            self.ev3_addr = None
+        else:
+            self.ev3_addr = (addr, port)
+
+        self.message_index = 0
+        self.condition = threading.Condition()
+        self.msg_times = {}
+        self.msg_results = {}
+
+        self.thread = threading.Thread(target=self.run)
+        self.thread.daemon = True
+        self.thread.start()
+
+
+    def wait_for_connection(self):
+        while self.ev3_addr is None:
+            time.sleep(0.01)
+
+    def run(self):
+        while True:
+            data, addr = self.socket.recvfrom(1024)
+            if self.ev3_addr is None:
+                self.ev3_addr = addr
+            self.parse_data(data)
+
+    def parse_data(self, data):
+        r = struct.unpack('HHHH', data[:8])
+        self.last_fn = data[8:8 + r[2]].strip()
+        self.last_data = data[8 + r[2]:].strip()
+
+        if r[1] == self.message_index:
+            with self.condition:
+                self.condition.notify()
+
+
+    def send(self, type, index, fn, data_len=None, data=None, msg_period=None):
+        if self.ev3_addr is None:
+            print 'Still looking for EV3'
+            return
+        if msg_period is not None:
+            last_time = self.msg_times.get(fn, None)
+            now = timeit.default_timer()
+            if last_time is not None and now < last_time + msg_period:
+                return
+            self.msg_times[fn] = now
+
+        if data is not None:
+            data_len = len(data)
+
+        header = struct.pack('HHHH', type, index, len(fn)+1, data_len)
+        message = header + fn + '\x00'
+        if data is not None:
+            message = message + data
+        self.socket.sendto(message, self.ev3_addr)
+
+    def request(self, type, fn, data=None, data_len=None, blocking=True,
+                msg_period=None):
+
+        if msg_period is not None:
+            last_time = self.msg_times.get(fn, None)
+            now = timeit.default_timer()
+            if last_time is not None and now < last_time + msg_period:
+                return self.msg_results.get(fn, None)
+            self.msg_times[fn] = now
+
+        self.message_index += 1
+        if blocking:
+            with self.condition:
+                self.send(type, self.message_index, fn, 
+                          data=data, data_len=data_len)
+                self.condition.wait()
+            self.msg_results[fn] = self.last_data
+            return self.last_data
+        else:
+            self.send(type, self.message_index, fn, 
+                      data=data, data_len=data_len)
+
+    def dir(self, fn, data_len=100, msg_period=None):
+        return self.request(5, fn, data_len=data_len, msg_period=msg_period)
+
+    def read(self, fn, data_len=1000, msg_period=None):
+        return self.request(3, fn, data_len=data_len, msg_period=msg_period)
+
+    def write(self, fn, data, msg_period=None, blocking=False):
+        self.request(1, fn, data=data, blocking=blocking,
+                     msg_period=msg_period)
+
+
+if __name__ == '__main__':
+    link = EV3Link('10.42.0.3')
+    link.wait_for_connection()
+    print link.dir('/sys/class/tacho-motor')
+    link.write('/sys/class/tacho-motor/motor0/command', 'run-direct')
+
+    data = []
+
+    now = time.time()
+    for i in range(500):
+        p = 0
+        p = link.read('/sys/class/tacho-motor/motor0/position')
+        t = time.time() - now
+        data.append((t, float(p)))
+        value = '%d' % (30 * math.sin(t*math.pi*2))
+        link.write('/sys/class/tacho-motor/motor0/duty_cycle_sp', data=value)
+        time.sleep(0.0001)
+    print time.time() - now
+
+    time.sleep(0.1)
+    link.write('/sys/class/tacho-motor/motor0/duty_cycle_sp', data='0')
+
+    import pylab
+    data = np.array(data)
+    pylab.plot(data[:,0], data[:,1])
+    pylab.show()
